26a049b1c895a27f2295a25b62fb160b3c80bf6d
raghuvarun1221 <56993248+raghuvarun1221@users.noreply.github.com>
2021-08-26 00:18:30 +0530

    Add files via upload

---
+++ libraries/commandMenu.js
@@ -0,0 +1,15 @@
+const commandMenuChoices = [
+    
+    "Create new Employee",
+    "Create new Department",
+    "View all employees",
+    "view all employees by department",
+    "Link employees to Department",
+    "View employees age",
+    "Delete an employee",
+    "Delete a Department",
+     //This was not used in menu option because it when role is updated the department changes automatically
+      
+]
+
+module.exports = commandMenuChoices
